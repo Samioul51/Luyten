9801347f91212628dacfd5b82e0d5ba48887b515
Samioul51 <akmsamioulislam51@gmail.com>
2025-08-20 14:10:15 +0600

    Student java class added with getters,setters and constructor

--- src/us/deathmarine/luyten/Student.java
+++ src/us/deathmarine/luyten/Student.java
@@ -4,14 +4,17 @@ public class Student{
     private String name;
     private int id;
     private double gpa;
+
     public Student(){
 
     }
+
     public Student(String name,int id,double gpa){
         this.name=name;
         this.id=id;
         this.gpa=gpa;
     }
+
     public String getName(){ 
         return name; 
     }
