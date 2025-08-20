c3c8ac1b7d8b258167eea5b8ae06b5510bfd63d3
Samioul51 <akmsamioulislam51@gmail.com>
2025-08-20 18:33:31 +0600

    Added Grade.java with simple grading

---
+++ src/us/deathmarine/Grade.java
@@ -0,0 +1,15 @@
+package us.deathmarine;
+
+public class Grade {
+    public static String gradeScore(int score){
+        if(score>=90) 
+            return "A";
+        if(score>=80) 
+            return "B";
+        if(score>=70) 
+            return "C";
+        if(score>=60) 
+            return "D";
+        return "F";
+    }
+}
