f1ae89da04982226966df763fb8f179cf756b244
Samioul51 <akmsamioulislam51@gmail.com>
2025-08-20 14:14:05 +0600

    Added Calculator.java with basic operations

---
+++ src/us/deathmarine/luyten/Calculator.java
@@ -0,0 +1,20 @@
+package us.deathmarine.luyten;
+
+public class Calculator {
+    public int add(int a,int b){ 
+        return (a+b); 
+    }
+    public int subtract(int a,int b)
+    { 
+        return (a-b); 
+    }
+    public int multiply(int a,int b){ 
+        return (a*b); 
+    }
+    public double divide(double a,double b)
+    {
+        if (b == 0)
+            throw new ArithmeticException("Division by zero");
+        return (a/b);
+    }
+}
